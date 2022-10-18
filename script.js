a1c883f93596c2317f5a2d0c6f929974039e8a23
kpijenifer <jenifermech7@gmail.com>
2022-10-19 00:01:33 +0530

    add-file.

--- script.js
+++ script.js
@@ -1,158 +1,140 @@
+document.getElementById("start").addEventListener("click", stage1Oprtn);
+let problemsEle = document.getElementsByClassName("problems")[0];
+let calculateEle = document.getElementById("calculate");
 
-function stage1(){
-    let opr=document.getElementById("operator");
-    let num=document.getElementById("number");
-     let randomnumber1;
-     let randomnumber2;
-    let operator=opr.value,
-    number=parseInt(num.value);
-}
-    
+
+function stage1Oprtn() {
+  let operationEle = document.getElementById("operation"),
+    rangeEle = document.getElementById("range"),
+    randomNumber1,
+    randomNumber2,
+    operation = operationEle.value,
+    range = parseInt(rangeEle.value);
 
 function stage2(){
-  let progresss_1=document.getElementsByClassName("progress");
-  let optEle,optype,num1Ele,num1Elevalue,num2Elevalue,num2Ele,expected,actual;
-  for(i=0;i<progress_1.length;i++){
-    let progresss_1=progresss_1[i];
-    optEle=progresss_1.document.getElementById("operator");
-    optype=optEle.innerHTML;
-    num1Ele=progresss_1.document.getElementById("number1");
-    num1Elevalue=parseInt(num1Ele.innerHTML);
-    num2Ele=progresss_1.document.getElementById("number2");
-    num2Elevalue=parseInt(num2Ele.innerHtml);
+    let problems_1=document.getElementsByClassName("problem"),
+    opEle,opType,nu1Ele,nu2Ele,nu2Elevalue,nu1Elevalue,expected,actual,actualAnswer,mark=0;
+    for(i=0;i<problems_1.length;i++){
+       var problem_1=problems_1[i];
+       //findoperation
+        opEle=problem_1.getElementsByClassName("oprtn")[0];
+        opType=opEle.innerHTML;
+        //console.log(opType)
 
-    if(optype=="+"){
-      expected=num1Elevalue+num2ELevalue;
+       
+       //get number1 and number2 value
+        nu1Ele=problem_1.getElementsByClassName("number1")[0];
+        nu1Elevalue=parseInt(nu1Ele.innerHTML); //console.log(nu1Elevalue);
+
+        nu2Ele=problem_1.getElementsByClassName("number2")[0];
+        nu2Elevalue=parseInt(nu2Ele.innerHTML);
+        //console.log(nu2Elevalue);
+
+       //find expected answer
+       if(opType=="+"){
+        expected=nu1Elevalue + nu2Elevalue;
+       }
+       else if(opType=="-"){
+        expected=nu1Elevalue - nu2Elevalue;
+       }
+       else if(opType=="*"){
+        expected=nu1Elevalue * nu2Elevalue;
+       }
+       else{
+        expected=nu1Elevalue / nu2Elevalue;
+       } 
+       console.log(expected);
+       //get actual answer
+       actual=problem_1.getElementsByClassName("answer")[0].value;
+       actualAnswer=parseInt(actual.value);
+       console.log(actualAnswer);
+       //compare the answer
+       if(expected == actualAnswer){
+            mark++;
+       }
+       //if true add mark
     }
-    else if(optype=="-"){
-      expectesd=num1Elevalue-num2ELevalue;
-    }
-    else if(optype=="-"){
-      expected=num1Elevalue*num2ELevalue;
-    }
-    else{
-      expected=num1Elevalue/number2Elevalue
-    }
-    console.log(expected);
-    
-    
-    
-    
-    acutal=problems_1.document.getElementById("answer")[0];
-    actualanswer=parseInt(acutal.value);
-    if(expected==actualanswer){
-      mark++;
-    }
+    alert("your mark is"+mark)
+}
+calculateEle.addEventListener("click",stage2);
+  resetproblemsEle();
+  for (i = 1; i < 16; i++) {
+    randomNumber1 = generateRandomNumber(range);
+    randomNumber2 = generateRandomNumber(range);
+
+    updateProblem(randomNumber1, randomNumber2, operation);
   }
-  alert("this is your"+answer);
-}
-function generateRandomnumber(num){
-   return parseInt(Math.random()*num);
-}
-
-function enableCalculate(){
-      calculateEle.removeAttribute("Disabled");
-}
-function textmo(text){
-  
-switch(text){
-
-    case "add": 
-        return "+";
-        break;
-
-   case "sub": 
-   return "-";
-        break;
-     case "mul":
-     return "*" ;
-     break; 
-     
-     default:                             ``
-        return "/";
-        break;
-      }  
-}
-function resetprogress(){
-  let progresEle=document.getElementsByClassName("progress")[0];
-   progresEle.innerHtml="";
-  }
-   function updateprogress(number1,number2,operator){
-   let progressEle=document.createElement("div");
-   progressEle.setAttribute("class","progress");
-    let number1Ele=document.createElement("h3");
-   number1Ele.setAttribute("class","number1");
-    let number2Ele=document.createElement("h3");
-   number2Ele.setAttribute("class","number2")
-    let operationEle=document.createElement("h3");
-   operationEle.setAttribute("class","operator");
-
- operationEle.innerText=textmo(operation);
- inptEle=document.createElement("input");
- inptEle.setAttribute("class","answer");
- inptEle.setAttribute("tyoe","text");
-
-
- progressEle.append(number1Ele);
- progressEle.append(operationEle);
- progressEle.append(number2Ele);
- progressEle.append(inptEle);
-  
-//  progresssEle.append(progressEle);
- progressEle.append(progresssEle);
-
- }
-
-
-
-let len=document.getElementById("start").addEventListener("click",stage1);
-let progressEle=document.getElementsByClassName("progress")[0];
-let calculateEle=document.getElementById("calculate");
-let operation=document.getElementById('operation')
-
- 
-calculateEle.addEventListener("click",stage2),
-     resetprogress();
-for(i=0;i<16;i++){
-      randomnumber1=generateRandomnumber(number);
-      randomnumber2=generateRandomnumber(number);
-  
-  
-    updateprogress(randomnumber1,randomnumber2,operator)
-   }
   enableCalculate();
+}
+function generateRandomNumber(max) {
+  return parseInt(Math.random() * max);
+}
+function enableCalculate() {
+  calculateEle.removeAttribute("Disabled");
+}
+function texttosymbol(text) {
+  switch (text) {
+    case "add":
+      return "+";
+      break;
+    case "sub":
+      return "-";
+      break;
+    case "multiplication":
+      return "*";
+      break;
+    default:
+      return "/";
+      break;
+  }
+}
+function resetproblemsEle() {
+  problemsEle.innerHTML = "";
+}
+function updateProblem(number1, number2, operation) {
+  // create Problem div
+  let problemEle = document.createElement("div");
+  problemEle.setAttribute("class", "problem");
 
+  // Create h3 for number1
+  let number1Ele = document.createElement("h3");
+    number1Ele.setAttribute("class","number1");
+  // update value foe number1
+  number1Ele.innerText = number1;
+  //console.log(number1Ele);
 
+  // Create h3 for number2
+  let number2Ele = document.createElement("h3");
+  number2Ele.setAttribute("class","number2");
 
+  // update value for number2
+  number2Ele.innerText = number2;
+  //console.log(number2Ele);
 
+  // Create h3 for operation
+  let oprtnEle = document.createElement("h3");
+  oprtnEle.setAttribute("class","oprtn");
 
+  // update symbol for Operation
+  oprtnEle.innerText = texttosymbol(operation);
+  //console.log(oprtnEle);
 
+  // create input box
+  let inptEle = document.createElement("input");
 
+  //Update input class name
+  inptEle.setAttribute("class", "answer");
 
+  // create input type text
+  inptEle.setAttribute("type", "text");
 
+  // append(add)hr,h3's,input inside problem
 
+  problemEle.append(number1Ele);
+  problemEle.append(oprtnEle);
+  problemEle.append(number2Ele);
+  problemEle.append(inptEle);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  // Append Inside Problems
+  problemsEle.append(problemEle);
+}
